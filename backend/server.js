a7979dade3a3d6647e8db33124fe337c8e6cef19
NGouv <natagouv@gmail.com>
2026-09-16 15:25:38 -0300

    Configura porta do Railway

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const chamadoRoutes = require("./routes/chamadoRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // ==============================
